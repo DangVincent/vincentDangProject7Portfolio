527f04ba94f3544c236d0e7b36be2c0fb25ea49e
Vincent Dang <vncntdang@gmail.com>
2019-12-16 15:36:16 -0500

    added script file :smiley_cat:

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const myPortfolioApp = {};
+
+myPortfolioApp.buttonEvent() {
+    
+}
+
+myPortfolioApp.init() = function() {
+    myPortfolioApp.buttonEvent();
+}
+
+$(function() {
+    myPortfolioApp.init();
+});
