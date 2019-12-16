7531c1e21cb79b7c2818e45a40223ab03e0e1955
Vincent Dang <vncntdang@gmail.com>
2019-12-16 16:48:28 -0500

    fixed a bug in the script file :smiley_cat:

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 const myPortfolioApp = {};
 
+// Link event function that controls the smooth scroll behaviour
+myPortfolioApp.linkEvent = function() {
+    // scroll behaviour obtained from https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
+    const $navLink = $(".header__navItem a[href^='#']");  
+    const $scrollLink = $(".header__content a[href^='#']");
+
+    $navLink.add($scrollLink).on('click', function(e) {
+        e.preventDefault();
+
+        var position = $($(this).attr('href')).offset().top;
+
+        $("body, html").animate({
+            scrollTop: position
+        }, 500, 'swing');
+    });
+}
+
 myPortfolioApp.buttonEvent = function() {
     
 }
 
 myPortfolioApp.init = function() {
     myPortfolioApp.buttonEvent();
+    myPortfolioApp.linkEvent();
 }
 
 $(function() {
